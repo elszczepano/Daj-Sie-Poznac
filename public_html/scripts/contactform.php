04c4b30217d2e97970e3cea9851d584b04264dce
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-03-11 15:57:21 +0100

    3/11/2017

--- public_html/scripts/contactform.php
+++ public_html/scripts/contactform.php
@@ -1,15 +1,9 @@
-<html>
-    <head>
-        <meta http-equiv="refresh" content="0" url="../index.html">﻿
-    </head>
-    <body>
         <?php
+        header('Location: ../index.html');
         $headers = 'From: '.$_POST['name'].' e-mail: '.$_POST['email'].'\r\n'.
                 'Content-Type: text/html; charset=utf-8';
-        $to = 'kontakt@dszczepaniak.pl';
+        $to = 'xyrex54@gmail.com';
         $subject = $_POST['subject'].'\n';
         $message = $_POST['content'].'\n';
         mail($to, $subject, $message, $headers);
         ?>
-    </body>
-</html>
